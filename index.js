7fccff27c6828171c1b6d8b45eafedf26ac8ae04
NYHolzer <nissan.holzer@gmail.com>
2019-08-05 20:21:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    newDriver = {...driver} 
+    newDriver.key = value
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver.key = value
+}
+
