771bcce56d4ae4695178df88b688d7e93eac7728
NYHolzer <nissan.holzer@gmail.com>
2019-08-05 20:28:46 -0400

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,23 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-    newDriver = {...driver} 
-    newDriver.key = value
+    const newDriver = {...driver} 
+    newDriver[key] = value
+    return newDriver
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-    driver.key = value
+    driver[key] = value
+    return driver
 }
 
+function deleteFromDriverByKey(driver, key) {
+    const newDriver = {...driver}
+    delete newDriver[key]
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}
